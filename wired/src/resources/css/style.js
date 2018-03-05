213510eb74c4f6855fdd06cfb7cd32e2b93294fb
kenji SUZUKI <kenji.suzuki@uzabase.com>
2018-03-06 00:43:23 +0900

    styleを微修正 + ソーシャルメディアの位置をリサイズに追随できるように

--- wired/src/resources/css/style.js
+++ wired/src/resources/css/style.js
@@ -1,8 +1,25 @@
 require("./style.css");
 
+const getElement = (css) => document.querySelector(css);
+
+const getArticleContainer = () => getElement(".article-container");
+
+const getSocialContainer = () => getElement(".social-container");
+
+const isOver = () => window.scrollY > 437;
+
+const calcSocialContainerLeft = () => {
+    const articleX = getArticleContainer().getBoundingClientRect().x;
+    return isOver() ? `${articleX - 137}px` : "-137px";
+};
+
 window.addEventListener("scroll", (e) => {
-    const style = document.querySelector(".social-container").style;
-    const isOver = window.scrollY > 460
-    style.top = isOver ? "77px" : "540px";
-    style.position = isOver ? "fixed" : "absolute";
+    const style = getSocialContainer().style;
+    style.top = isOver() ? "108px" : "0px";
+    style.position = isOver() ? "fixed" : "absolute";
+    style.left = calcSocialContainerLeft();
+});
+
+window.addEventListener("resize", (e) => {
+    getSocialContainer().style.left = calcSocialContainerLeft();
 });
