4d644b6c6c490959e304b412c22a4403095d10a1
kenji SUZUKI <kenji.suzuki@uzabase.com>
2018-04-23 15:02:16 +0900

    cumbyの続き

--- cumby/src/resources/css/style.js
+++ cumby/src/resources/css/style.js
@@ -1,2 +1,17 @@
 require("./style.css");
 
+window.addEventListener("scroll", (e) => {
+    const y = window.scrollY;
+    const logo = document.querySelector(".header");
+    const menu = document.querySelector(".menu");
+    const stiky = document.querySelector(".header .sticky");
+    if (y === 0) {
+        logo.classList.remove("has-sticky");
+        menu.classList.remove("has-sticky");
+        stiky.classList.remove("on");
+    } else {
+        logo.classList.add("has-sticky");
+        menu.classList.add("has-sticky");
+        stiky.classList.add("on");
+    }
+});
